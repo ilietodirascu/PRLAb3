ef629de1ad181f8154441039046d8b6f7780a142
ilietodirascu <ilie.todirascu@isa.utm.md>
2022-11-09 19:47:20 +0200

    first commit

---
+++ Lab3/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Controllers
+{
+    public class UsersController : ControllerBase
+    {
+        private Memory _memory = Memory.GetMemory();
+        [HttpGet]
+        [Route("/GetUsers")]
+        public IEnumerable<User> GetUsers()
+        {
+            return _memory.Users.ToList();
+        }
+        [HttpPost]
+        [Route("/AddUser")]
+        public IActionResult AddUser(User user)
+        {
+            if (_memory.Users.Any(x => x.Id == user.Id))
+            {
+                return Forbid();
+            }
+            _memory.Users.Add(user);
+            return Ok();
+        }
+        [HttpDelete]
+        [Route("/DeleteUserById")]
+        public IActionResult DeleteUserById(int id)
+        {
+            var user = _memory.Users.Find(x => x.Id == id);
+            if (user is null) return NotFound();
+            _memory.Users.Remove(user);
+            return Ok();
+        }
+        [HttpPut]
+        [Route("/UpdateUser")]
+        public IActionResult UpdateUser(int oldId,User newUser)
+        {
+            var userIndex = _memory.Users.FindIndex(x => x.Id == oldId);
+            if (userIndex < 0) return NotFound();
+            _memory.Users[userIndex] = newUser;
+            return Ok();
+        }
+    }
+}
---
+++ Lab3/Memory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Memory
+    {
+        public List<User> Users { get; set; }
+        private static readonly Memory _memory = new();
+        private Memory()
+        {
+            Users = new();
+        }
+        public static Memory GetMemory()
+        {
+            return _memory;
+        }
+    }
+}
